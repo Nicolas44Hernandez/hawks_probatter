a93b2e907d4294898d424cedb75e52b9d7c7f8dd
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-03-31 11:36:38 +0200

    keyboard input WIP

--- hawks_batter_server/server/interfaces/keyboard_input_interface/service.py
+++ hawks_batter_server/server/interfaces/keyboard_input_interface/service.py
@@ -3,7 +3,6 @@ KeyboardInputInterface interface service
 """
 import logging
 import threading
-import cv2
 from pynput import keyboard 
 
 waiting_ms = 100
@@ -35,58 +34,23 @@ class KeyboardInputInterface(threading.Thread):
 
         # Start keyboard listener          
         listener = keyboard.Listener(on_press=self.on_press)
-        listener.start()
-
-            # keyboard.on_press_key("c", lambda _:self.setup_image())
-            # keyboard.on_press_key("r", lambda _:self.new_game())
-            # keyboard.on_press_key("q", lambda _:self.exit_game())
-            # keyboard.on_press_key("n", lambda _:self.set_new_video())
-
-        # # Call Super constructor
-        # super(KeyboardInputInterface, self).__init__(name="KeyboardInputInterfaceThread")
-        # self.setDaemon(True)
-
+        listener.start()       
     def on_press(self, key):
         """Callback for key pressed"""
         logger.info(f"key pressed {key}")
-        # try:
-        #     print('alphanumeric key {0} pressed'.format(
-        #         key.char))
-        # except AttributeError:
-        #     print('special key {0} pressed'.format(
-        #         key))
+        if key('c'):  
+            self.setup_callback()
+            return
+        if key('r'):  
+            self.run_callback()
+            return
+        if key('q'):  
+            self.exit_callback()
+            return
+        if key('n'):  
+            self.new_video_callback()
+            return
 
-    # def run(self):
-    #     """Run thread"""   
-    #     while True:      
-    #         logger.info("Loop")
-    #         try:  
-    #             if keyboard.is_pressed('c'):  
-    #                 self.setup_callback()
-    #                 continue 
-    #             if keyboard.is_pressed('r'):  
-    #                 self.run_callback()
-    #                 continue 
-    #             if keyboard.is_pressed('q'):  
-    #                 self.exit_callback()
-    #                 continue 
-    #             if keyboard.is_pressed('n'):  
-    #                 self.new_video_callback()
-    #                 continue 
-    #         except:
-    #             logger.info("Exception")
-    #             continue  
-    #         # pressed_key =  cv2.waitKey(waiting_ms)
-    #         # if pressed_key:
-    #         #     if pressed_key == ord('c'):
-    #         #         self.setup_callback()
-    #         #     elif pressed_key == ord('r'):
-    #         #         self.run_callback()
-    #         #     elif pressed_key == ord('q'):
-    #         #         self.exit_callback()
-    #         #     elif pressed_key == ord('n'):
-    #         #         self.new_video_callback()
-        
-
+  
     
     
