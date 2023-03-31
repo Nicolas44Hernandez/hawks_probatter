378a5c5d69e9f749eca3eaf3399913543933d9fb
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-03-31 10:44:02 +0200

    keyboard input WIP

--- hawks_batter_server/server/interfaces/keyboard_input_interface/service.py
+++ hawks_batter_server/server/interfaces/keyboard_input_interface/service.py
@@ -4,6 +4,9 @@ KeyboardInputInterface interface service
 import logging
 import threading
 import cv2
+import sys
+import select
+import time
 
 interframe_wait_ms = 30
 
@@ -37,17 +40,33 @@ class KeyboardInputInterface(threading.Thread):
         self.setDaemon(True)
 
     def run(self):
-        """Run thread"""      
+        """Run thread"""  
+        
+            
         
         while True:      
-            pressed_key =  cv2.waitKey(interframe_wait_ms)
-            if pressed_key:
-                if pressed_key == ord('c'):
-                    self.setup_callback()
-                elif pressed_key == ord('r'):
-                    self.run_callback()
-                elif pressed_key == ord('q'):
-                    self.exit_callback()
-                elif pressed_key == ord('n'):
-                    self.new_video_callback()
+            input = select.select([sys.stdin], [], [], 1)[0]
+            if input:
+                pressed_key = sys.stdin.readline().rstrip()
+                logger.info(f"pressed key {pressed_key}")
+        
+                # if (pressed_key == "q"):
+                #     print "Exiting"
+                #     sys.exit(0)
+                # else:
+                #     print "You entered: %s" % value
+            else:
+                logger.info("noting pressed")
+                time.sleep(0.2)
+
+            # pressed_key =  cv2.waitKey(interframe_wait_ms)
+            # if pressed_key:
+            #     if pressed_key == ord('c'):
+            #         self.setup_callback()
+            #     elif pressed_key == ord('r'):
+            #         self.run_callback()
+            #     elif pressed_key == ord('q'):
+            #         self.exit_callback()
+            #     elif pressed_key == ord('n'):
+            #         self.new_video_callback()
         
