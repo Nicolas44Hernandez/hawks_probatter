46ecb6267b5b54b1633c70f0ec56d31449c7c796
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-03-31 11:32:34 +0200

    keyboard input WIP

--- hawks_batter_server/server/interfaces/keyboard_input_interface/service.py
+++ hawks_batter_server/server/interfaces/keyboard_input_interface/service.py
@@ -4,7 +4,7 @@ KeyboardInputInterface interface service
 import logging
 import threading
 import cv2
-import keyboard 
+from pynput import keyboard 
 
 waiting_ms = 100
 
@@ -33,40 +33,59 @@ class KeyboardInputInterface(threading.Thread):
         self.exit_callback=exit_callback
         self.new_video_callback=new_video_callback
 
-        # Call Super constructor
-        super(KeyboardInputInterface, self).__init__(name="KeyboardInputInterfaceThread")
-        self.setDaemon(True)
+        # Start keyboard listener          
+        listener = keyboard.Listener(on_press=self.on_press)
+        listener.start()
 
-    def run(self):
-        """Run thread"""   
-        while True:      
-            logger.info("Loop")
-            try:  
-                if keyboard.is_pressed('c'):  
-                    self.setup_callback()
-                    continue 
-                if keyboard.is_pressed('r'):  
-                    self.run_callback()
-                    continue 
-                if keyboard.is_pressed('q'):  
-                    self.exit_callback()
-                    continue 
-                if keyboard.is_pressed('n'):  
-                    self.new_video_callback()
-                    continue 
-            except:
-                logger.info("Exception")
-                continue  
-            # pressed_key =  cv2.waitKey(waiting_ms)
-            # if pressed_key:
-            #     if pressed_key == ord('c'):
-            #         self.setup_callback()
-            #     elif pressed_key == ord('r'):
-            #         self.run_callback()
-            #     elif pressed_key == ord('q'):
-            #         self.exit_callback()
-            #     elif pressed_key == ord('n'):
-            #         self.new_video_callback()
+            # keyboard.on_press_key("c", lambda _:self.setup_image())
+            # keyboard.on_press_key("r", lambda _:self.new_game())
+            # keyboard.on_press_key("q", lambda _:self.exit_game())
+            # keyboard.on_press_key("n", lambda _:self.set_new_video())
+
+        # # Call Super constructor
+        # super(KeyboardInputInterface, self).__init__(name="KeyboardInputInterfaceThread")
+        # self.setDaemon(True)
+
+    def on_press(self, key):
+        """Callback for key pressed"""
+        logger.info(f"key pressed {key}")
+        # try:
+        #     print('alphanumeric key {0} pressed'.format(
+        #         key.char))
+        # except AttributeError:
+        #     print('special key {0} pressed'.format(
+        #         key))
+
+    # def run(self):
+    #     """Run thread"""   
+    #     while True:      
+    #         logger.info("Loop")
+    #         try:  
+    #             if keyboard.is_pressed('c'):  
+    #                 self.setup_callback()
+    #                 continue 
+    #             if keyboard.is_pressed('r'):  
+    #                 self.run_callback()
+    #                 continue 
+    #             if keyboard.is_pressed('q'):  
+    #                 self.exit_callback()
+    #                 continue 
+    #             if keyboard.is_pressed('n'):  
+    #                 self.new_video_callback()
+    #                 continue 
+    #         except:
+    #             logger.info("Exception")
+    #             continue  
+    #         # pressed_key =  cv2.waitKey(waiting_ms)
+    #         # if pressed_key:
+    #         #     if pressed_key == ord('c'):
+    #         #         self.setup_callback()
+    #         #     elif pressed_key == ord('r'):
+    #         #         self.run_callback()
+    #         #     elif pressed_key == ord('q'):
+    #         #         self.exit_callback()
+    #         #     elif pressed_key == ord('n'):
+    #         #         self.new_video_callback()
         
 
     
