d61b4463c5e465aef9c72c3d8ae09a8b26e0cce7
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-12 11:05:25 +0200

    set as a service + access point

--- web_server/src/config.js
+++ web_server/src/config.js
@@ -1,8 +1,8 @@
 const config = {
-    IMAGE_SETUP_URL: 'http://192.168.1.85:5000/configuration/image_setup',
-    VIDEOS_LIST_URL: 'http://192.168.1.85:5000/configuration/videos_list', 
-    CONFIGURATION_URL: 'http://192.168.1.85:5000/configuration',
-    RUNNING_URL: 'http://192.168.1.85:5000/running',
+    IMAGE_SETUP_URL: 'http://192.168.4.1:5000/configuration/image_setup',
+    VIDEOS_LIST_URL: 'http://192.168.4.1:5000/configuration/videos_list', 
+    CONFIGURATION_URL: 'http://192.168.4.1:5000/configuration',
+    RUNNING_URL: 'http://192.168.4.1:5000/running',
   };
   
   export default config;
