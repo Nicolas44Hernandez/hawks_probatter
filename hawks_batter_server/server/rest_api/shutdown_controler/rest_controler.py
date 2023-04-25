ddab6ecdb1a8c9ee5452a8d119ae865e645b3269
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-25 15:56:39 +0200

    Shutdown api in server

---
+++ hawks_batter_server/server/rest_api/shutdown_controler/rest_controler.py
@@ -0,0 +1,25 @@
+""" REST controller for machine manager ressource """
+import logging
+import os
+from flask.views import MethodView
+from flask_smorest import Blueprint
+
+logger = logging.getLogger(__name__)
+
+bp = Blueprint("shutdown", __name__, url_prefix="/shutdown")
+""" The api blueprint. Should be registered in app main api object """
+
+
+@bp.route("/")
+class MachineManagementApi(MethodView):
+    """API to manage machine status"""
+
+    @bp.doc(
+        responses={400: "BAD_REQUEST", 404: "NOT_FOUND"},
+    )
+    @bp.response(status_code=200)
+    def get(self):
+        """Shutdown RPI"""
+
+        logger.info(f"GET shutdown/")
+        return os.system("shutdown -h now")
