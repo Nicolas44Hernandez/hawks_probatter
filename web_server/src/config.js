214e0cab4e2158e73359a6ce20bd89d9a67cd9c0
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-25 16:36:04 +0200

    shutdown in HMI

--- web_server/src/config.js
+++ web_server/src/config.js
@@ -3,6 +3,7 @@ const config = {
     VIDEOS_LIST_URL: 'http://192.168.4.1:5000/configuration/videos_list', 
     CONFIGURATION_URL: 'http://192.168.4.1:5000/configuration',
     RUNNING_URL: 'http://192.168.4.1:5000/running',
+    SHUTDOWN_URL: 'http://192.168.4.1:5000/shutdown',
   };
   
   export default config;
