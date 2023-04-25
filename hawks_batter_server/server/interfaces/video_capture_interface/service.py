9f0478b386a82dd54f1400fe4a8936fd38f206ab
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-25 12:45:57 +0200

    video load and play modification

--- hawks_batter_server/server/interfaces/video_capture_interface/service.py
+++ hawks_batter_server/server/interfaces/video_capture_interface/service.py
@@ -3,6 +3,7 @@ Video capture interface service
 """
 import logging
 import threading
+from typing import Iterable
 import cv2
 import numpy
 import os
@@ -15,14 +16,15 @@ logger = logging.getLogger(__name__)
 class VideoCaptureInterface(threading.Thread):
     """Service class for video manager interface management"""
 
-    capture: cv2.VideoCapture
-    fps: int
+    video: str
+    interframe_time: int
     running: bool
     setting_up: bool
     waiting_for_start: bool
     waiting_for_pitch_frame: numpy.ndarray
     startup_frame: numpy.ndarray
     setup_frame: numpy.ndarray    
+    video_frames: Iterable[numpy.ndarray]
 
     def __init__(self, video:str, setup_frame: numpy.ndarray, startup_frame: numpy.ndarray):  
         logger.info("Video manager interface started") 
@@ -41,22 +43,22 @@ class VideoCaptureInterface(threading.Thread):
             logger.error("Impossible to open startup frame")
             return 
         self.startup_frame = captured_startup_frame        
-        
+                
         # Set initial values
+        self.video = video
         self.running = False
         self.setting_up = False
-        self.waiting_for_start = True   
-        self.capture = cv2.VideoCapture(video)
+        self.waiting_for_start = True  
+        self.interframe_time = None 
 
-        # Get first frame
-        ret, frame = self.capture.read()
-        if ret:
-            self.waiting_for_pitch_frame = frame
-            self.fps = self.capture.get(cv2.CAP_PROP_FPS)
-        else: 
+        # Load video frames
+        self.video_frames = self.load_video(video)
+        if self.video_frames is None:
             logger.error("Impossible to open video")
-            return        
-        logger.info(f"Playing video {video} fps:{self.fps}")
+            return 
+        
+        # Get first frame 
+        self.waiting_for_pitch_frame = self.video_frames[0]
 
         # Call Super constructor
         super(VideoCaptureInterface, self).__init__(name="VideoCaptureInterfaceThread")
@@ -64,32 +66,34 @@ class VideoCaptureInterface(threading.Thread):
 
     def run(self):
         """Run thread"""      
-        time.sleep(10)
+        time.sleep(5)
         cv2.namedWindow(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN)
         cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
 
+        current_frame_pos = 0 
         while True:   
             if self.setting_up:
                 self.running = False
                 self.waiting_for_start = True  
-                cv2.imshow(WINDOW_NAME, self.setup_frame)             
+                cv2.imshow(WINDOW_NAME, self.setup_frame) 
+                current_frame_pos = 0            
             else:   
                 if not self.running:
                     cv2.imshow(WINDOW_NAME, self.startup_frame)  
                     self.waiting_for_start = True
+                    current_frame_pos = 0 
                 else:
-                    if not self.waiting_for_start:                  
-                        ret, frame = self.capture.read()
-                        if not ret:
-                            logger.info("Reached end of video")
-                            self.capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
+                    if not self.waiting_for_start:   
+                        current_frame_pos = current_frame_pos + 1
+                        if current_frame_pos > len(self.video_frames) - 1 :
                             self.waiting_for_start = True
-                            continue  
+                            continue
+                        frame = self.video_frames[current_frame_pos]
                         cv2.imshow(WINDOW_NAME, frame)
                     else:                    
-                        self.capture.set(cv2.CAP_PROP_POS_FRAMES, 0)
                         cv2.imshow(WINDOW_NAME, self.waiting_for_pitch_frame)
-            cv2.waitKey(30)
+                        current_frame_pos = 0 
+            cv2.waitKey(self.interframe_time)
 
     def start_game(self):
         """Start game"""
@@ -118,18 +122,15 @@ class VideoCaptureInterface(threading.Thread):
             return False
         self.stop_video()
         self.running = False
-        self.capture.release()
-        time.sleep(2)
-        self.capture = cv2.VideoCapture(video)
-         # Get first frame
-        ret, frame = self.capture.read()
-        if ret:
-            self.waiting_for_pitch_frame = frame
-            self.fps = self.capture.get(cv2.CAP_PROP_FPS)            
-        else: 
+
+        # Load video frames
+        self.video_frames = self.load_video(video)
+        if not self.video_frames is None:
             logger.error("Impossible to open video")
-            return False  
-        logger.info(f"Playing video {video} fps:{self.fps}")
+            return False
+        
+        # Get first frame 
+        self.waiting_for_pitch_frame = self.video_frames[0]
         return True    
 
     def stop_video(self):
@@ -154,3 +155,37 @@ class VideoCaptureInterface(threading.Thread):
             logger.error("Impossible to open setup frame")
         cap.release()
         return frame
+
+    def load_video(self, video: str):
+        """Load video in array"""
+
+        logger.info(f"Loading video: {video}")
+
+        cap = cv2.VideoCapture(video)
+        fps = cap.get(cv2.CAP_PROP_FPS) 
+        total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        interframe_time = int(1000/fps)
+
+        logger.info(f"Video fps: {fps}")
+        logger.info(f"Total frames: {total}")
+        logger.info(f"Interframe time: {interframe_time}")
+
+        video_frames = []
+
+        # Check if video opened successfully
+        if (cap.isOpened()== False):
+            logger.info("Error opening video file")
+            return None
+        
+        # Read until video is complete
+        while(cap.isOpened()):      
+            ret, frame = cap.read()
+            if ret == True:
+                video_frames.append(frame)  
+            else:
+                break
+        
+        cap.release()
+        logger.info(f"Total frames in video: {len(video_frames)}")
+        self.interframe_time = interframe_time
+        return video_frames                
