090495ef0e90d8e8669048baa6b263f64d174c2f
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-26 16:34:54 +0200

    webservice dependencies in setup script

--- setup/scripts/flask-server-setup.sh
+++ setup/scripts/flask-server-setup.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+LINE="export PATH="$PATH:/home/pi/.local/bin""
 
 echo "SETUP FOR FLASK SERVER"
 
@@ -7,4 +8,11 @@ echo "********STAGE 1: INSTALL FLASK SERVER DEPENDENCIES ********"
 sudo apt-get install -y python3-pip
 cd ../../
 pip install -r hawks_batter_server/requirements.txt
-echo export PATH="$PATH:/home/pi/.local/bin" >> ~/.bashrc
+echo 'export PATH="\$PATH:/home/pi/.local/bin"' >> ~/.bashrc
+
+echo "********STAGE 2: INSTALL WEB SERVER DEPENDENCIES ********"
+cd ~
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+nvm install node
+nvm alias default 19.9.0
+sudo apt install build-essential
