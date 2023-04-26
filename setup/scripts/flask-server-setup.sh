237fb02e319b5641baebca04b4a8771b6957167c
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-26 16:39:42 +0200

    webservice dependencies in setup script

--- setup/scripts/flask-server-setup.sh
+++ setup/scripts/flask-server-setup.sh
@@ -13,6 +13,9 @@ echo 'export PATH="\$PATH:/home/pi/.local/bin"' >> ~/.bashrc
 echo "********STAGE 2: INSTALL WEB SERVER DEPENDENCIES ********"
 cd ~
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install node
 nvm alias default 19.9.0
 sudo apt install build-essential
