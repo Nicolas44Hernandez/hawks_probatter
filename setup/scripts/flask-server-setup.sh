93f0c5723205166955d9483ab97c0e467b07ce4d
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-26 16:22:35 +0200

    Flask server automatic setup script

--- setup/scripts/flask-server-setup.sh
+++ setup/scripts/flask-server-setup.sh
@@ -5,6 +5,10 @@ echo "SETUP FOR FLASK SERVER"
 
 echo "********STAGE 1: INSTALL FLASK SERVER DEPENDENCIES ********"
 sudo apt-get install -y python3-pip
+echo antes
+pwd
 cd ../../hawks_batter_server
+echo desues
+pwd
 pip install -r hawks_batter_server/requirements.txt
 echo export PATH="$PATH:/home/pi/.local/bin" >> ~/.bashrc
