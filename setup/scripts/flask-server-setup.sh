30e90c5b54ce2edab204df7851f386bc47bdca5e
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-27 09:51:54 +0200

    probatter as a service

--- setup/scripts/flask-server-setup.sh
+++ setup/scripts/flask-server-setup.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 LINE="export PATH="$PATH:/home/pi/.local/bin""
+NGINX_CONFIG_FILE="/etc/nginx/sites-available/default"
 
-echo "SETUP FOR FLASK SERVER"
+echo "SETUP FOR PROBATTER APP"
 
 
 echo "********STAGE 1: INSTALL FLASK SERVER DEPENDENCIES ********"
@@ -10,7 +11,16 @@ cd ../../
 pip install -r hawks_batter_server/requirements.txt
 echo 'export PATH="\$PATH:/home/pi/.local/bin"' >> ~/.bashrc
 
-echo "********STAGE 2: INSTALL WEB SERVER DEPENDENCIES ********"
+echo "********STAGE 2: CREATE LOG FILES ********"
+mkdir logs
+mkdir logs/manager
+mkdir logs/interface
+touch logs/app.log 
+touch logs/rest_api.log 
+touch logs/manager/video.log logs/interface/video_capture.log
+
+echo "********STAGE 3: INSTALL WEB SERVER DEPENDENCIES ********"
+sudo apt install -y nginx
 cd ~
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
@@ -20,7 +30,27 @@ nvm install node
 nvm alias default 19.9.0
 sudo apt install build-essential
 
-echo "********STAGE 2: INSTALL WEB SERVER ********"
+
+echo "********STAGE 4: INSTALL WEB SERVER ********"
 cd -
 cd web_server
 npm install
+
+
+echo "********STAGE 5: SET FLASK APP AS A SERVICE ********"
+cd ../hawks_batter_server
+sudo cp services/probatter.service /etc/systemd/system/
+sudo systemctl daemon-reload
+sudo systemctl enable probatter
+sudo systemctl restart probatter
+
+echo "********STAGE 6: SET WEBSERVICE AS A SERVICE ********"
+sudo cp $NGINX_CONFIG_FILE $NGINX_CONFIG_FILE.orig
+echo "Old file backup in: $NGINX_CONFIG_FILE.orig"
+sudo cp $RESOURCES/nginx-config $NGINX_CONFIG_FILE
+echo "File created : $NGINX_CONFIG_FILE"
+sudo systemctl restart nginx
+
+
+echo "********STAGE 7: REBOOT********"
+sudo reboot now
