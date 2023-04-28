b980ebe83ffc74d20cbc09950f28c1c8929063a6
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-04-28 09:42:16 +0200

    WIP: only nginx in script

--- setup/scripts/probatter-install.sh
+++ setup/scripts/probatter-install.sh
@@ -6,20 +6,20 @@ RESOURCES=setup/resources
 echo "SETUP FOR PROBATTER APP"
 
 
-echo "********STAGE 1: INSTALL FLASK SERVER DEPENDENCIES ********"
-sudo apt-get install -y python3-pip
-cd ../../
-pip install -r hawks_batter_server/requirements.txt
-echo 'export PATH="\$PATH:/home/pi/.local/bin"' >> ~/.bashrc
+# echo "********STAGE 1: INSTALL FLASK SERVER DEPENDENCIES ********"
+# sudo apt-get install -y python3-pip
+# cd ../../
+# pip install -r hawks_batter_server/requirements.txt
+# echo 'export PATH="\$PATH:/home/pi/.local/bin"' >> ~/.bashrc
 
-echo "********STAGE 2: CREATE LOG FILES ********"
-mkdir logs
-mkdir logs/manager
-mkdir logs/interface
-touch logs/app.log 
-touch logs/rest_api.log 
-touch logs/manager/video.log logs/interface/video_capture.log
-sudo chmod -R 777 logs
+# echo "********STAGE 2: CREATE LOG FILES ********"
+# mkdir logs
+# mkdir logs/manager
+# mkdir logs/interface
+# touch logs/app.log 
+# touch logs/rest_api.log 
+# touch logs/manager/video.log logs/interface/video_capture.log
+# sudo chmod -R 777 logs
 
 echo "********STAGE 3: INSTALL WEB SERVER DEPENDENCIES ********"
 sudo apt install -y nginx
@@ -40,12 +40,12 @@ npm install
 npm run build
 
 
-echo "********STAGE 5: SET FLASK APP AS A SERVICE ********"
-cd ../
-sudo cp services/probatter.service /etc/systemd/system/
-sudo systemctl daemon-reload
-sudo systemctl enable probatter
-sudo systemctl restart probatter
+# echo "********STAGE 5: SET FLASK APP AS A SERVICE ********"
+# cd ../
+# sudo cp services/probatter.service /etc/systemd/system/
+# sudo systemctl daemon-reload
+# sudo systemctl enable probatter
+# sudo systemctl restart probatter
 
 echo "********STAGE 6: SET WEBSERVICE AS A SERVICE ********"
 sudo cp $NGINX_CONFIG_FILE $NGINX_CONFIG_FILE.orig
@@ -55,5 +55,5 @@ echo "File created : $NGINX_CONFIG_FILE"
 sudo systemctl restart nginx
 
 
-echo "********STAGE 7: REBOOT********"
-sudo reboot now
+# echo "********STAGE 7: REBOOT********"
+# sudo reboot now
