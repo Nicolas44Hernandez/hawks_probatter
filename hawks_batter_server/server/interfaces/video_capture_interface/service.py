8e48fa2e882b6bd3fa04a30a2cae4059d9cd3698
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-07-03 16:44:45 +0200

    WIP: interframe=1

--- hawks_batter_server/server/interfaces/video_capture_interface/service.py
+++ hawks_batter_server/server/interfaces/video_capture_interface/service.py
@@ -116,7 +116,8 @@ class VideoCaptureInterface(threading.Thread):
                             self.draw_text(frame, text)                                     
                         cv2.imshow(WINDOW_NAME, frame)
                         current_frame_pos = 0 
-            cv2.waitKey(self.interframe_time-10)
+            cv2.waitKey(1)
+            #cv2.waitKey(self.interframe_time-10)
 
     def start_game(self):
         """Start game"""
