2c74bae0bbe76a9ce5c7965b0bc026495d12e5b0
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-07-05 10:21:54 +0200

    update restart services

--- web_server/src/components/MachineButtonsItem.vue
+++ web_server/src/components/MachineButtonsItem.vue
@@ -43,7 +43,7 @@ export default {
       } 
     },
     onClickReboot() {
-      if (window.confirm('Are you sure that you want to reboot system?')) {
+      if (window.confirm('Are you sure that you want to restart services?')) {
         this.$emit('reboot');
       } 
     }
