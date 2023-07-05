5cf590230baefc7bf54fdebf8368829ac0e2ec8f
Nicolas Hernandez <n44hernandezp@gmail.com>
2023-07-05 10:51:46 +0200

    sensors to buttons

--- hawks_batter_server/server/managers/sensors_manager/service.py
+++ hawks_batter_server/server/managers/sensors_manager/service.py
@@ -1,7 +1,7 @@
 import logging
 import time
 from flask import Flask
-from server.interfaces.gpio_interface.service import GpioMotionSensorInterface
+from server.interfaces.gpio_interface.service import GpioButtonInterface
 from server.managers.video_manager import video_manager_service
 
 logger = logging.getLogger(__name__)
@@ -10,8 +10,8 @@ logger = logging.getLogger(__name__)
 class SensorsManager:
     """Manager for Sensors detection peripheral"""
 
-    sensor_1_interface: GpioMotionSensorInterface
-    sensor_1_interface: GpioMotionSensorInterface
+    button_1_interface: GpioButtonInterface
+    button_2_interface: GpioButtonInterface
 
     def __init__(self, app: Flask = None) -> None:
         if app is not None:
@@ -22,30 +22,27 @@ class SensorsManager:
         if app is not None:
             logger.info("initializing the SensorsManager")
 
-            self.sensor_1_interface = GpioMotionSensorInterface(
-                sensor_pin=app.config["SENSOR_START_PIN"],
-                callback_function=self.sensor_start_callback,
+            self.button_1_interface = GpioButtonInterface(
+                button_pin=app.config["SENSOR_START_PIN"],
+                callback_function=self.start_callback,
             )
 
-            self.sensor_2_interface = GpioMotionSensorInterface(
-                sensor_pin=app.config["SENSOR_STOP_PIN"],
-                callback_function=self.sensor_end_callback,
+            self.button_2_interface = GpioButtonInterface(
+                button_pin=app.config["SENSOR_STOP_PIN"],
+                callback_function=self.end_callback,
             )
 
-    def sensor_start_callback(self, channel):
-        """Callback function for sensor 1 detection"""
+    def start_callback(self, channel):
+        """Callback function for button 1 detection"""
         # Sensor debounce
         time.sleep(0.2)
         video_manager_service.run_video()
     
-    def sensor_end_callback(self, channel):
-        """Callback function for sensor 2 detection"""
+    def end_callback(self, channel):
+        """Callback function for button 2 detection"""
         # Sensor debounce
         time.sleep(0.2)
-        video_manager_service.stop_video()
-
-
-        
+        video_manager_service.stop_video()        
 
 
 sensors_manager_service: SensorsManager = SensorsManager()
