a55c69447b794df189f12474bd1dd2857bd9cf7e
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-02-12 11:47:27 +0100

    test buttons scripts

---
+++ hawks_batter_server/test/test_buttons.py
@@ -0,0 +1,15 @@
+from gpiozero import Button
+
+
+def callback_function_start():
+    print("Button start pressed")
+
+
+def callback_function_end():
+    print("Button end pressed")
+
+
+button_start = Button(23)
+button_end = Button(24)
+button_start.when_pressed = callback_function_start
+button_end.when_pressed = callback_function_end
