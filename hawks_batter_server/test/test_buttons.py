56a01752bbbb674dc06cb1c84593280a2627fe20
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-02-12 12:50:32 +0100

    WIP

--- hawks_batter_server/test/test_buttons.py
+++ hawks_batter_server/test/test_buttons.py
@@ -13,7 +13,7 @@ def callback_function_end():
 
 
 #button_start = Button(23)
-button_end = Button(24)
+button_end = Button(24, bounce_time = 1)
 #button_start.when_pressed = callback_function_start
 button_end.when_pressed = callback_function_end
 
