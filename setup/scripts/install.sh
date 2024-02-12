51065e16216c00e4b85996e687c48c966951bce8
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-02-12 11:47:27 +0100

    test buttons scripts

--- setup/scripts/install.sh
+++ setup/scripts/install.sh
@@ -5,7 +5,7 @@ echo "Install.........................."
 
 sudo chmod 755 opencv-installation.sh 
 sudo chmod 755 probatter-install.sh 
-sudo chmod 755 access-point-setup.sh 
+#sudo chmod 755 access-point-setup.sh 
 
 echo "------------------------------- INSTALLING OPENCV -------------------------------"
 sudo ./opencv-installation.sh
@@ -13,8 +13,8 @@ sudo ./opencv-installation.sh
 echo "------------------------------- INSTALLING PROBATTER -------------------------------"
 sudo ./probatter-install.sh 
 
-echo "------------------------------- SETTING AS WIFI ACCESS POINT -------------------------------"
-sudo ./access-point-setup.sh
+# echo "------------------------------- SETTING AS WIFI ACCESS POINT -------------------------------"
+# sudo ./access-point-setup.sh
 
 echo Rebooting.....
 sudo reboot
