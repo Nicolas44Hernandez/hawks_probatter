6f92ea2342d1955e7976680fcdad61996d0415f7
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-02-14 10:49:03 +0100

    WIP

--- hawks_batter_server/test/test_buttons.py
+++ hawks_batter_server/test/test_buttons.py
@@ -2,18 +2,20 @@ from gpiozero import Button
 import time
 
 
-def callback_function(button_number):
+def start_callback_function(button_number):
     print(f"Button pressed: {button_number}")
     print(f"pin: {button_number.pin}")
-    print(f"type(pin): {type(button.pin)}")
+    print(f"type(pin): {type(button_number.pin)}")
     if button_number.pin == 23:
         print("button_number == 23")
     time.sleep(1)
 
 
 
-button = Button(23) 
-button.when_pressed = callback_function
+button_start = Button(23) 
+button_end = Button(24) 
+button_start.when_pressed = start_callback_function
+button_end.when_pressed = start_callback_function
 
 
 while True: 
