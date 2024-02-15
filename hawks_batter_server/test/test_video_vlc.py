08a9a43b6814a93be4b05d89ad2d37b933961f97
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-02-15 11:07:52 +0100

    vlc

--- hawks_batter_server/test/test_video_vlc.py
+++ hawks_batter_server/test/test_video_vlc.py
@@ -1,12 +1,42 @@
 import vlc
+import time
 
 VIDEO_1 = "../../app_data/videos/jules_windup.mp4"
 VIDEO_2 = "../../app_data/videos/jules_slide_step.mp4"
 
 
  
-# creating vlc media player object
-media = vlc.MediaPlayer(VIDEO_1)
  
-# start playing video
-media.play()
+ 
+# method to play video
+def video(source):
+     
+    # creating a vlc instance
+    vlc_instance = vlc.Instance()
+     
+    # creating a media player
+    player = vlc_instance.media_player_new()
+     
+    # creating a media
+    media = vlc_instance.media_new(source)
+     
+    # setting media to the player
+    player.set_media(media)
+     
+    # play the video
+    player.play()
+     
+    # wait time
+    time.sleep(0.5)
+     
+    # getting the duration of the video
+    duration = player.get_length()
+     
+    # printing the duration of the video
+    print("Duration : " + str(duration))
+     
+# call the video method
+video(VIDEO_1)
+
+while True: 
+    a = 4
