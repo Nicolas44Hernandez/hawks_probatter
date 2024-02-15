f52aa0f32fe8cab4eca5f477f5ddd2cf9141dfa3
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-02-15 10:19:51 +0100

    test videos

---
+++ hawks_batter_server/test/test_video.py
@@ -0,0 +1,102 @@
+"""
+Video capture interface service
+"""
+import threading
+from typing import Iterable
+import cv2
+import numpy
+import os
+import time
+
+WINDOW_NAME = "VIDEO TEST HAWKS BASEBALL"
+VIDEO_TO_PLAY = "/home/pi/workspace/hawks_probatter/app_data/videos/jules_windup.mp4"
+
+
+class VideoCaptureInterface(threading.Thread):
+    """Service class for video manager interface management"""
+
+    video: str
+    interframe_time: int
+    video_frames: Iterable[numpy.ndarray]
+
+    def __init__(self, video:str):  
+        print("Video manager interface started") 
+        print(f"Video: {video}")                 
+                
+        # Set initial values
+        self.video = video
+        self.interframe_time = None 
+
+        # Load video frames
+        self.video_frames = None
+        self.video_frames = self.load_video(video)
+        if self.video_frames is None:
+            print("ERROR: Impossible to open video")
+            return 
+        
+        # Get first frame 
+        self.waiting_for_pitch_frame = self.video_frames[0]
+
+        # Call Super constructor
+        super(VideoCaptureInterface, self).__init__(name="VideoCaptureInterfaceThread")
+        self.setDaemon(True)
+
+    def run(self):
+        """Run thread"""      
+        time.sleep(5)
+        # cv2.namedWindow(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN)
+        # cv2.setWindowProperty(WINDOW_NAME, cv2.WND_PROP_FULLSCREEN, cv2.WINDOW_FULLSCREEN)
+
+        current_frame_pos = 0 
+        while True:                
+            current_frame_pos = current_frame_pos + 1
+            if current_frame_pos > len(self.video_frames) - 1 :
+                return
+            raw_frame = self.video_frames[current_frame_pos]  
+            frame = raw_frame.copy()                                              
+            print(f"frame {current_frame_pos}")
+            cv2.imshow(WINDOW_NAME, frame)
+            #cv2.waitKey(int(self.interframe_time/2))
+            cv2.waitKey(8)
+
+    def load_video(self, video: str):
+        """Load video in array"""
+
+        print(f"Loading video: {video}")
+
+        cap = cv2.VideoCapture(video)
+        fps = cap.get(cv2.CAP_PROP_FPS) 
+        total = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        interframe_time = int(1000/fps)
+
+        print(f"Video fps: {fps}")
+        print(f"Total frames: {total}")
+        print(f"Interframe time: {interframe_time}")
+
+        video_frames = []
+
+        # Check if video opened successfully
+        if (cap.isOpened()== False):
+            print("Error opening video file")
+            return None
+        
+        # Read until video is complete
+        while(cap.isOpened()):      
+            ret, frame = cap.read()
+            if ret == True:
+                video_frames.append(frame)  
+            else:
+                break
+        
+        cap.release()
+        print(f"Total frames in video: {len(video_frames)}")
+        self.interframe_time = interframe_time
+        return video_frames                
+    
+
+def run_video(video: str):
+    video_capture_interface = VideoCaptureInterface(video=video)
+    video_capture_interface.run()
+
+if __name__ == '__main__':
+    run_video(video=VIDEO_TO_PLAY)
