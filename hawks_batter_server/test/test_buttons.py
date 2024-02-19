ec1783fe76ab1f87e6d5639019326f1e697ba4cd
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-02-19 15:17:43 +0100

    wip

--- hawks_batter_server/test/test_buttons.py
+++ hawks_batter_server/test/test_buttons.py
@@ -21,8 +21,8 @@ def end_callback_function():
 
 
 
-button_start = Button(23) 
-button_end = Button(24) 
+button_start = Button(2) 
+#button_end = Button(24) 
 #button_start.when_pressed = start_callback_function
 #button_end.when_pressed = end_callback_function
 running = False
@@ -30,8 +30,8 @@ running = False
 while True: 
     if button_start.is_pressed:
         print("Button start is pressed")
-    if button_end.is_pressed:
-        print("Button end is pressed")
+    # if button_end.is_pressed:
+    #     print("Button end is pressed")
     #time.sleep(1)
     #print("Waitting...")
     #a =2
