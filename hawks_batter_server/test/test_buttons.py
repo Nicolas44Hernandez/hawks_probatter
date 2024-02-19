5d1a9688150e369efbc13596e404478422f4fa45
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-02-19 16:02:39 +0100

    WIP

--- hawks_batter_server/test/test_buttons.py
+++ hawks_batter_server/test/test_buttons.py
@@ -5,16 +5,14 @@ global running
 
 def start_callback_function():
     global running
-    #if not running:
-    if True:
+    if not running:
         print(f"Button start pressed")
         print(f"Waitting for end button press")
         running = True
 
 def end_callback_function():
     global running
-    #if running:
-    if True:    
+    if running:  
         print(f"Button end pressed")
         print(f"Waitting for start button press")
         running = False
@@ -22,9 +20,9 @@ def end_callback_function():
 
 
 button_start = Button(23) 
-#button_end = Button(24) 
+button_end = Button(24) 
 button_start.when_pressed = start_callback_function
-#button_end.when_pressed = end_callback_function
+button_end.when_pressed = end_callback_function
 running = False
 
 pause()
