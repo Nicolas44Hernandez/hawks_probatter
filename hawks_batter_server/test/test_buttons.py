249c0a990258cdc3918929641d01b1e4019dddcb
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-02-19 15:16:37 +0100

    waitting in test buttons

--- hawks_batter_server/test/test_buttons.py
+++ hawks_batter_server/test/test_buttons.py
@@ -22,12 +22,16 @@ def end_callback_function():
 
 
 button_start = Button(23) 
-#button_end = Button(24) 
-button_start.when_pressed = start_callback_function
+button_end = Button(24) 
+#button_start.when_pressed = start_callback_function
 #button_end.when_pressed = end_callback_function
 running = False
 
 while True: 
+    if button_start.is_pressed:
+        print("Button start is pressed")
+    if button_end.is_pressed:
+        print("Button end is pressed")
     #time.sleep(1)
     #print("Waitting...")
-    a =2
+    #a =2
