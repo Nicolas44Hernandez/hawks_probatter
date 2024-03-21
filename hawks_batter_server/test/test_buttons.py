516b52486feff50d9e4c4242c8f95b4eab4db36f
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-03-21 10:07:42 +0100

    test buttons

--- hawks_batter_server/test/test_buttons.py
+++ hawks_batter_server/test/test_buttons.py
@@ -1,29 +1,16 @@
 from gpiozero import Button
 from signal import pause
+import time
 
-global running
 
 def start_callback_function():
-    global running
-    if not running:
-        print(f"Button start pressed")
-        print(f"Waitting for end button press")
-        running = True
-
-def end_callback_function():
-    global running
-    if running:  
-        print(f"Button end pressed")
-        print(f"Waitting for start button press")
-        running = False
+    time.sleep(0.2)
+    print(f"Button start pressed")
 
 
 
 button_start = Button(23) 
-button_end = Button(24) 
 button_start.when_pressed = start_callback_function
-button_end.when_pressed = end_callback_function
-running = False
 
 pause()
 # while True: 
