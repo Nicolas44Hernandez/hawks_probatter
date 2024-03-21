a464a1982f476f69ca070e2b550989c930d3c1bd
Nicolas Hernandez <n44hernandezp@gmail.com>
2024-03-21 10:00:44 +0100

    test button disable

--- hawks_batter_server/server/managers/sensors_manager/service.py
+++ hawks_batter_server/server/managers/sensors_manager/service.py
@@ -11,6 +11,7 @@ class SensorsManager:
     """Manager for Sensors detection peripheral"""
 
     button_1_interface: GpioButtonInterface
+    start_button_pin: int
 
     def __init__(self, app: Flask = None) -> None:
         if app is not None:
@@ -20,21 +21,35 @@ class SensorsManager:
         """Initialize SensorsManager"""
         if app is not None:
             logger.info("initializing the SensorsManager")
+            
+            self.start_button_pin = app.config["SENSOR_START_PIN"]
 
-            self.button_1_interface = GpioButtonInterface(
-                button_pin=app.config["SENSOR_START_PIN"],
-                callback_function=self.start_callback,
-            )
+            self.enable_button()
+            
 
     def start_callback(self):
         """Callback function for button 1 detection"""
         # Sensor debounce
         logger.info("start button callback")
+        self.disable_button()
         time.sleep(0.2)
         if video_manager_service.video_capture_interface.waiting_for_start:
             video_manager_service.start_pitch()      
             time.sleep(3)
+            self.enable_button()
 
+
+    def disable_button(self):
+        """Disable button"""
+        self.button_1_interface = None
+
+
+    def enable_button(self):
+        """Enable button"""
+        self.button_1_interface = GpioButtonInterface(
+            button_pin=self.start_button_pin,
+            callback_function=self.start_callback,
+        )
 
 sensors_manager_service: SensorsManager = SensorsManager()
 """ Sensors manager service singleton"""
